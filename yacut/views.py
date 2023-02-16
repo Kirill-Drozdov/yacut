5dc8b6212216446c9fc5bc7d343537a9e443e628
Kirill Drozdov <kirill.drozdov1998@yandex.ru>
2023-02-16 19:01:43 +0500

    Add form

--- yacut/views.py
+++ yacut/views.py
@@ -1,8 +1,27 @@
-from flask import render_template
+from flask import render_template, flash
 
-from . import app
+from yacut import app, db
+from yacut.forms import URLForm
+from yacut.models import URLMap
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index_view():
-    return render_template('index.html')
+    form = URLForm()
+    if form.validate_on_submit():
+        short = form.short.data
+        original = form.original.data
+        if URLMap.query.filter_by(short=short).first():
+            flash('Такая короткая ссылка уже существует!')
+            return render_template('index.html', form=form)
+        elif URLMap.query.filter_by(original=original).first():
+            flash('Такая длинная ссылка уже существует!')
+            return render_template('index.html', form=form)
+        url = URLMap(
+            original=form.original.data,
+            short=form.short.data,
+        )
+        db.session.add(url)
+        db.session.commit()
+        return render_template('index.html', form=form)
+    return render_template('index.html', form=form)
