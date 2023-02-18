64094f1c9238b0cc5b33c026d3182dfdf453669e
Kirill Drozdov <kirill.drozdov1998@yandex.ru>
2023-02-18 09:36:33 +0500

    Modify form field names

--- yacut/forms.py
+++ yacut/forms.py
@@ -4,11 +4,11 @@ from wtforms.validators import DataRequired, Length, Optional
 
 
 class URLForm(FlaskForm):
-    original = URLField(
+    original_link = URLField(
         'Длинная ссылка',
         validators=[DataRequired(message='Обязательное поле')]
     )
-    short = StringField(
+    custom_id = StringField(
         'Ваш вариант короткой ссылки',
         validators=[Length(1, 16), Optional()]
     )
--- yacut/views.py
+++ yacut/views.py
@@ -9,27 +9,22 @@ from yacut.models import URLMap
 def add_url_view():
     form = URLForm()
     if form.validate_on_submit():
-        short = form.short.data
-        original = form.original.data
+        short = form.custom_id.data
         if URLMap.query.filter_by(short=short).first():
             flash('Такая короткая ссылка уже существует!')
             return render_template('add_url.html', form=form)
-        elif URLMap.query.filter_by(original=original).first():
-            flash('Такая длинная ссылка уже существует!')
-            return render_template('add_url.html', form=form)
         url = URLMap(
-            original=form.original.data,
-            short=form.short.data,
+            original=form.original_link.data,
+            short=form.custom_id.data,
         )
         db.session.add(url)
         db.session.commit()
         return render_template('add_url.html', form=form)
-        # return redirect(url_for('url_detail_view', id=url.id))
     return render_template('add_url.html', form=form)
 
 
-@app.route('/<path:short>')
-def redirect_view(short):
-    url = URLMap.query.filter_by(short=short).first()
+@app.route('/<path:custom_id>')
+def redirect_view(custom_id):
+    url = URLMap.query.filter_by(short=custom_id).first()
     original = url.original
     return redirect(original)
