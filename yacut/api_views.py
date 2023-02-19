96dcf3c70f2f9028b455e2304b40b0a03c97ebd2
Kirill Drozdov <kirill.drozdov1998@yandex.ru>
2023-02-19 19:30:30 +0500

    Fix tests

--- yacut/api_views.py
+++ yacut/api_views.py
@@ -9,19 +9,19 @@ from yacut.error_handlers import InvalidAPIUsage
 @app.route('/api/id/', methods=['POST'])
 def create_id_view():
     data = request.get_json()
-    if 'url' not in data:
-        raise InvalidAPIUsage('"url" является обязательным полем!')
-    # if 'custom_id' not in data and 'url' not in data:
-    #     raise InvalidAPIUsage('Отсутствует тело запроса')
-    if 'custom_id' not in data:
+    if data is None:
+        raise InvalidAPIUsage('Отсутствует тело запроса')
+    custom_id = data.get('custom_id')
+    if custom_id is None or custom_id == '':
         custom_id = get_unique_short_id()
         data['custom_id'] = custom_id
-    custom_id = data['custom_id']
+    if 'url' not in data:
+        raise InvalidAPIUsage('"url" является обязательным полем!')
     if URLMap.query.filter_by(short=custom_id).first() is not None:
         raise InvalidAPIUsage(f'Имя "{custom_id}" уже занято.')
-    if len(data['custom_id']) > 16:
+    if len(custom_id) > 16:
         raise InvalidAPIUsage('Указано недопустимое имя для короткой ссылки')
-    # data['custom_id'] = 'http://localhost/' + custom_id
+
     url = URLMap()
     url.from_dict(data)
     db.session.add(url)
